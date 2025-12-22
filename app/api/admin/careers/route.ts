2b186807ce64355d6b34db2bf16784ff08bb3361
hisham-globalsurf <hisham@globalsurf.ae>
2025-12-22 16:15:03 +0530

    feat:(backend console: careers-main-page)

--- app/api/admin/careers/route.ts
+++ app/api/admin/careers/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import connectDB from "@/lib/mongodb";
-import Career from "@/app/models/career";
+import Career from "@/app/models/Career";
 import { verifyAdmin } from "@/lib/verifyAdmin";
 
 
