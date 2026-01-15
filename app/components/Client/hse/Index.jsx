87cb119f7d84010fc93dc4767aacc8370cf975b8
Nived <nived@globalsurf.ae>
2026-01-15 11:51:21 +0530

    style:hse page changes

--- app/components/Client/hse/Index.jsx
+++ app/components/Client/hse/Index.jsx
@@ -3,7 +3,10 @@ import CoreValues from "./sections/CoreValues";
 // import EnsuringSafety from "./sections/EnsuringSafety";
 import Certifications from "./sections/Certifications";
 import SaftySlider from "./sections/SaftySlider";
+import Philosophy from "./sections/Philosophy";
+import Environmental from "./sections/Environmental";
 import ImgPointsComponent from "@/app/components/common/ImgPointsComponent";
+import { environmentalData } from "./sections/data";
 const QualitySafety = ({ data }) => {
     return (
         <>
@@ -12,6 +15,8 @@ const QualitySafety = ({ data }) => {
       </header> */}
             <Banner title={data.pageTitle} image={data.banner} imageAlt={data.bannerAlt} />
             <CoreValues data={data.firstSection} />
+            <Philosophy/>
+            <Environmental data={environmentalData}/>
             <Certifications data={data.secondSection} />
             <SaftySlider data={data.thirdSection} />
             <ImgPointsComponent data={data.fourthSection} bgColor="[#f5f5f5]" sectionSpacing="pt-text90 pb25" />
