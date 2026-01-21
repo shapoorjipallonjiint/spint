6159bbf66514bad6c48f144c798ed7fa646d2d1a
hisham-globalsurf <hisham@globalsurf.ae>
2026-01-21 13:26:42 +0530

    feat(global-presence: arabic implemented)

--- app/components/Client/GlobalPresence/sections/ExpandingHorizons.jsx
+++ app/components/Client/GlobalPresence/sections/ExpandingHorizons.jsx
@@ -6,8 +6,12 @@ import { motion, useScroll, useTransform } from "framer-motion";
 import { moveUp } from "../../../motionVarients";
 import { useRef } from "react";
 import Image from "next/image";
+import { useApplyLang } from "@/lib/applyLang";
+import useIsPreferredLanguageArabic from "@/lib/getPreferredLanguage";
 
 const ExpandingHorizons = ({ data }) => {
+    const t = useApplyLang(data);
+    const isArabic = useIsPreferredLanguageArabic();
     const sectionRef = useRef(null);
     const MotionImage = motion.create(Image);
 
@@ -16,25 +20,37 @@ const ExpandingHorizons = ({ data }) => {
         offset: ["start end", "end start"],
     });
     const shapeY = useTransform(shapeProgress, [0, 1], [-200, 200]);
+
     return (
         <section className="relative overflow-hidden pt-text25 pb30" ref={sectionRef}>
-            <div className="absolute bottom-[-10px] lg:bottom-0 right-0 lg:left-0 w-fit">
+            <div
+                className={`absolute bottom-[-10px] lg:bottom-0 w-fit ${
+                    isArabic ? "left-0 lg:right-0" : "right-0 lg:left-0"
+                }`}
+            >
                 <MotionImage
                     height={1200}
                     width={563}
                     style={{ y: shapeY }}
                     src={assets.mainShape2}
                     alt=""
-                    className="w-[150px] lg:w-[450px] 3xl:w-[563px] h-auto max-w-[788px] object-contain relative lg:left-[-100px]"
+                    className={`w-[150px] lg:w-[450px] 3xl:w-[563px] h-auto max-w-[788px] object-contain relative ${
+                        isArabic ? "lg:right-[-100px] -scale-x-100" : "lg:left-[-100px]"
+                    }`}
                 />
             </div>
+
             <div className="container">
                 <div>
                     <div>
-                        <div className="w-full lg:max-w-[65%]   2xl:max-w-[73%] 3xl:max-w-[67.84%] ml-auto ">
+                        <div
+                            className={`w-full lg:max-w-[65%] 2xl:max-w-[73%] 3xl:max-w-[67.84%] ${
+                                isArabic ? "mr-auto" : "ml-auto"
+                            }`}
+                        >
                             {/* <h2 className="text-60 font-light leading-[1.166666666666667] mb-50px max-w-[22ch]">{data.title}</h2> */}
                             <H2Title
-                                titleText={data.title}
+                                titleText={t.title}
                                 titleColor="black"
                                 marginClass="mb-4 md:mb-6 2xl:mb-50px"
                                 maxW="max-w-[22ch]"
@@ -48,7 +64,7 @@ const ExpandingHorizons = ({ data }) => {
                                     viewport={{ amount: 0.2, once: false }}
                                     className="mb-4 xl:mb-8 last:mb-0 text-19 lg:text-20 3xl:text-29 font-light leading-[1.35] text-paragraph max-w-[48ch]"
                                 >
-                                    {data.description}
+                                    {t.description}
                                 </motion.p>
                             }
                         </div>
