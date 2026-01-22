3a648dd2cc6d23ecff9bd81141338d971957e922
hisham-globalsurf <hisham@globalsurf.ae>
2026-01-22 17:59:22 +0530

    backend created for two new sections in quality page

--- app/components/Client/Quality/Index.jsx
+++ app/components/Client/Quality/Index.jsx
@@ -11,13 +11,13 @@ import QualityPractices from "./sections/QualityPractices";
 const Index = ({ data }) => {
     return (
         <>
-            <Banner title={data.pageTitle} image={data.banner} imageAlt={data.bannerAlt} data={data}/>
+            <Banner title={data.pageTitle} image={data.banner} imageAlt={data.bannerAlt} data={data} />
             <CoreValues data={data.firstSection} />
-            <UnifiedStandard/>
+            <UnifiedStandard data={data.unifiedStandardSection} />
             <QualityPractices data={data.fourthSection} bgColor="[#f5f5f5]" sectionSpacing="pt-text90 pb25" />
             <Certifications data={data.secondSection} />
             <SaftySlider data={data.thirdSection} />
-            <Continual/>
+            <Continual data={data.sixthSection} />
         </>
     );
 };
